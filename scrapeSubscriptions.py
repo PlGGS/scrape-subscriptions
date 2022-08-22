7ae35373cc656a0a3cf13618f2a0b8d755f0b6d9
PlGGS <bbor0422@gmail.com>
2022-08-22 03:32:13 -0500

    Added comments

--- scrapeSubscriptions.py
+++ scrapeSubscriptions.py
@@ -7,19 +7,25 @@ import sys
 from datetime import date
 from bs4 import BeautifulSoup
 
+#check whether the user has saved their subscriptions page before running the script
 if not (os.path.isfile('subs.html')):
     print('Press CTRL+S on your https://www.youtube.com/feed/subscriptions page and save the html contents into this directory as subs.html before running.')
     sys.exit()
 
 HTMLFileToBeOpened = open("subs.html", "r")
 contents = HTMLFileToBeOpened.read()
+
+#create new soup object based on html
 soup = BeautifulSoup(contents, 'html.parser')
 
+#create dictionary for subcriptions and their links
 subs = dict()
 
+#loop through all sidepanel items to find subscriptions along with their hrefs
 for a in soup.find_all('a', href=True, class_="yt-simple-endpoint style-scope ytd-guide-entry-renderer"):
     subs[a['title']] = str(a.get_attribute_list('href')[0])
 
+#remove unnecessary sidepanel items
 subs.pop('Home')
 subs.pop('Explore')
 subs.pop('Shorts')
@@ -50,15 +56,17 @@ subs.pop('Report history')
 subs.pop('Help')
 subs.pop('Send feedback')
 
+#sort subcriptions alphabetically based on channel name
 sorted_subNames = sorted(subs, key=str.lower)
 sorted_subs = dict()
 
 for sub in sorted_subNames:
   sorted_subs[sub] = subs[sub]
 
+#write subcriptions to file
 fileName = 'subscriptions ' + str(date.today()) + '.csv'
 with open(fileName, 'w') as f:
     for name, link in sorted_subs.items():
         f.write(name + ',' + link + "\n")
 
-print('Outputed subscriptions to ' + fileName + '.csv')
+print('Done! Wrote subscriptions to ' + fileName + '.csv')
